10f7e0383894190c4b1c1116792f5d1c6ec32897
Im Ye Seul <147143220+Imysss@users.noreply.github.com>
2025-04-08 19:35:04 +0900

    del: 안쓰는 스크립트 삭제

--- Assets/Scripts/MainTitle.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-public class MainTitle : MonoBehaviour
-{
-    public void Maintitle()
-    {
-        SceneManager.LoadScene("StartScene");
-    }
-}
--- Assets/Scripts/UI_StartScene.cs
+++ Assets/Scripts/UI_StartScene.cs
@@ -1,10 +1,35 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UI_StartScene : MonoBehaviour
 {
     public GameObject GameLevelTitle;
     public GameObject ui_ScorePopup;
+
+    public Button easyButton;
+    public Button normalButton;
+    public Button hardButton;
+
+    void Start()
+    {
+        if (Managers.Instance.unlockNormal)
+        {
+            normalButton.interactable = true;
+        }
+        else
+        {
+            normalButton.interactable = false;
+        }
+        if (Managers.Instance.unlockHard)
+        {
+            hardButton.interactable = true;
+        }
+        else
+        {
+            hardButton.interactable = false;
+        }
+    }
 
     public void Retry()
     {
@@ -19,5 +44,27 @@ public class UI_StartScene : MonoBehaviour
     public void PopupScore()
     {
         ui_ScorePopup.SetActive(true);
+    }
+
+    public void OnClickEasyButton()
+    {
+        Managers.Instance.gameType = GameLevel.Easy;
+        LoadMainScene();
+
+    }
+    public void OnClickNormalButton()
+    {
+        Managers.Instance.gameType = GameLevel.Normal;
+        LoadMainScene();
+    }
+    public void OnClickHardButton()
+    {
+        Managers.Instance.gameType = GameLevel.Hard;
+        LoadMainScene();
+    }
+
+    void LoadMainScene()
+    {
+        SceneManager.LoadScene("MainScene");
     }
 }
