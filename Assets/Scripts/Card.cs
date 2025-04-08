03b0ccb5c2ad7077a5b35af116cb707c11078b7f
jinjuho2 <jinjuho3@gmail.com>
2025-04-08 15:42:02 +0900

    AudioManager
    AudioManager

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -23,7 +23,6 @@ public class Card : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
         if (GameManager.Instance.gameType != GameLevel.Hidden)
         {
             return;
