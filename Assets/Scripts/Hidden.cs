9e07e57a179c0dd54dfd04b5eb972c53a3ada6fb
Jisooking <122238121+Jisooking@users.noreply.github.com>
2025-04-09 11:46:48 +0900

    hidden

--- Assets/Scripts/Hidden.cs
+++ Assets/Scripts/Hidden.cs
@@ -4,17 +4,8 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 public class Hidden : MonoBehaviour
 {
-<<<<<<< HEAD
-    float direction = 0.05f;
-
-    SpriteRenderer renderer;
-
-    void Start()
-=======
     void GotoHiddenStage()
->>>>>>> juho_branch
     {
-
         Debug.Log("히든 스테이지로 이동합니다!");
         Managers.Instance.gameType = GameLevel.Hidden;
         LoadMainScene();
@@ -22,23 +13,7 @@ public class Hidden : MonoBehaviour
 
     void LoadMainScene()
     {
-<<<<<<< HEAD
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
-
-            if (hit.collider != null)
-            {
-                Managers.Instance.gameType = GameLevel.Hidden;
-                SceneManager.LoadScene("MainScene");
-                Debug.Log("히든 스테이지로");
-            }
-        }
-    }
-=======
         SceneManager.LoadScene("MainScene");
     }
 
->>>>>>> juho_branch
 }
