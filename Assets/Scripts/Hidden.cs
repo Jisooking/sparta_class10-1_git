12a04c99a55830a47c4e35aa49baef556063e3b1
jinjuho2 <jinjuho3@gmail.com>
2025-04-09 12:17:51 +0900

    1

--- Assets/Scripts/Hidden.cs
+++ Assets/Scripts/Hidden.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
+
 public class Hidden : MonoBehaviour
 {
     void GotoHiddenStage()
--- Assets/Scripts/UI_StartScene.cs
+++ Assets/Scripts/UI_StartScene.cs
@@ -6,8 +6,35 @@ public class UI_StartScene : MonoBehaviour
 {
     public GameObject ui_GameModePopup;
     public GameObject ui_ScorePopup;
+<<<<<<< HEAD
     public GameObject ui_SpecialModePopup;
 
+=======
+
+    public Button easyButton;
+    public Button normalButton;
+    public Button hardButton;
+
+    void Start()
+    {
+        if (Managers.Instance.unlockNormal)
+        {
+            normalButton.interactable = true;
+        }
+        else
+        {
+            normalButton.interactable = false;
+        }
+        if (Managers.Instance.unlockHard)
+        {
+            hardButton.interactable = true;
+        }
+        else
+        {
+            hardButton.interactable = false;
+        }
+    }
+>>>>>>> parent of 2daeabb (Feat feat 스페셜 모드 선택 UI > 연결)
 
     public void Retry()
     {
@@ -24,9 +51,33 @@ public class UI_StartScene : MonoBehaviour
         ui_ScorePopup.SetActive(true);
     }
 
+<<<<<<< HEAD
     public void PopupSpeicalMode()
     {
         ui_SpecialModePopup.SetActive(true);
     }
 
+=======
+    public void OnClickEasyButton()
+    {
+        Managers.Instance.gameType = GameLevel.Easy;
+        LoadMainScene();
+
+    }
+    public void OnClickNormalButton()
+    {
+        Managers.Instance.gameType = GameLevel.Normal;
+        LoadMainScene();
+    }
+    public void OnClickHardButton()
+    {
+        Managers.Instance.gameType = GameLevel.Hard;
+        LoadMainScene();
+    }
+
+    void LoadMainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+>>>>>>> parent of 2daeabb (Feat feat 스페셜 모드 선택 UI > 연결)
 }
